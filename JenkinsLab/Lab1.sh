faf3eab206dd9660a7472d62ae03908c456ba4cf
root <root@ip-172-31-16-163.ec2.internal>
2026-07-25 05:40:02 +0000

    First Jenkins Lab 1

---
+++ JenkinsLab/Lab1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Jenkins Lab1"
+echo "================================="
+echo "Current  User : $(whoami)"
+echo "Hostname : $(hostname)"
+echo "Date : $(date)"
+echo "=======jenkins lab1 Completed========"
